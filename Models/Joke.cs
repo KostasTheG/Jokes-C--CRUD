dad69383e4e525cb0ee5a00bb1516eed76f3b2ec
KostasTheG <kosta1914@gmail.com>
2023-07-19 20:16:41 +0300

    Main

---
+++ Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿namespace Jokes.Models
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+        public string? jokeQuestion { get; set; }
+
+        public string? jokeAnswer { get; set; }
+        public Joke() { 
+        }
+
+        public Joke(string id, string jokeQuestion, string jokeAnswer)
+        {
+            Id = id;
+            this.jokeQuestion = jokeQuestion;
+            this.jokeAnswer = jokeAnswer;
+        }
+
+
+    }
+}
---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [Id]           NVARCHAR (450) NOT NULL,
+    [jokeQuestion] NVARCHAR (MAX) NULL,
+    [jokeAnswer]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
